eefca80b8768145dc3295b75bb97232859ed254f
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2021-08-13 11:48:15 +0200

    intial commit

---
+++ src/Components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Card() {
+    return (
+        <div>
+            <img alt='robots' src='https://robohash.org/test' />
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@mail.com</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Card
